113a91827b18474aeffe8905726ca040fb2d1423
Nick Ciraki <nicholas.ciraki@jdplc.com>
2024-01-30 12:57:45 +0000

    results logic added and functional

--- app.js
+++ app.js
@@ -40,26 +40,31 @@ app.post('/', async (req, res) => {
     let firstDepartureTime,
       firstArrivalTime,
       secondDepartureTime,
-      secondArrivalTime;
+      secondArrivalTime,
+      firstColour,
+      secondColour;
     if (stepsLength > 1) {
       firstDepartureTime =
-        travelData.data.routes[0].legs[0].steps[0].transit_details
-          .departure_time.text;
+        travelData.data.routes[0].legs[0].steps[0].transit_details.departure_time.text;
       firstArrivalTime =
-        travelData.data.routes[0].legs[0].steps[0].transit_details.arrival_time
-          .text;
+        travelData.data.routes[0].legs[0].steps[0].transit_details.arrival_time.text;
       secondDepartureTime =
-        travelData.data.routes[0].legs[0].steps[stepsLength - 1].transit_details
-          .departure_time.text;
+        travelData.data.routes[0].legs[0].steps[stepsLength - 1].transit_details.departure_time.text;
       secondArrivalTime =
-        travelData.data.routes[0].legs[0].steps[stepsLength - 1].transit_details
-          .arrival_time.text;
+        travelData.data.routes[0].legs[0].steps[stepsLength - 1].transit_details.arrival_time.text;
+      firstColour =
+        travelData.data.routes[0].legs[0].steps[0].transit_details.line.color
+      secondColour =
+        travelData.data.routes[0].legs[0].steps[stepsLength - 1].transit_details.line.color
       console.log('Rendering 1st result.ejs');
+      console.log(firstColour, secondColour);
       res.render('result.ejs', {
         firstDepart: firstDepartureTime,
         firstArrival: firstArrivalTime,
         secondDepart: secondDepartureTime,
         secondArrival: secondArrivalTime,
+        firstColour: firstColour,
+        secondColour: secondColour,
       });
     } else {
       firstDepartureTime =
@@ -68,12 +73,17 @@ app.post('/', async (req, res) => {
       firstArrivalTime =
         travelData.data.routes[0].legs[0].steps[0].transit_details.arrival_time
           .text;
+      firstColour =
+        travelData.data.routes[0].legs[0].steps[0].transit_details.line.color
       console.log('Rendering 2nd result.ejs');
+      console.log(firstColour, secondColour);
       res.render('result.ejs', {
         firstDepart: firstDepartureTime,
         firstArrival: firstArrivalTime,
         secondDepart: null,
         secondArrival: null,
+        firstColour: firstColour,
+        secondColour: null,
       });
     }
   } catch (error) {
